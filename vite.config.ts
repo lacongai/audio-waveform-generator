dc878d91fe0ace9c77bbca23dfff42be3d079f88
HenTaiz <thailanfreefire2000@gmail.com>
2026-08-05 02:17:28 +0700

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    host: true
+    host: true,
+    allowedHosts: 'all' // Cho phép tất cả host
+  },
+  preview: {
+    port: 3000,
+    host: true,
+    allowedHosts: 'all'
   },
   build: {
     outDir: 'dist',
